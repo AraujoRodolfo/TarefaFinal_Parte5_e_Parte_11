80a2e7be7ca1c5da4af56470426c81bad3d7bba6
guuhsant <guiegucastro1@gmail.com>
2022-12-09 12:28:19 -0300

    Exercicios 21, 31, 35 e 45; todos em formato '.cs'; ASS: Gustavo de Castro Santos - 1 DES

---
+++ exe31.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            Calc();
+            Console.ReadKey();
+            }
+        static void Calc()
+        {
+            int par = 0;
+            int impar = 0;
+            int positivo = 0;
+            int negativo = 0;
+            int[] num = new int[10];
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine("Digite qualquer número");
+                num[i] = int.Parse(Console.ReadLine());
+                Console.Clear();
+            }
+            for (int j = 0; j < num.Length; j++)
+            {
+                if (num[j] % 2 == 0)
+                {
+                    par++;
+                }
+                else
+                {
+                    impar++;
+                }
+                if (num[j] >= 0)
+                {
+                    positivo++;
+                }
+                else
+                {
+                    negativo++;
+                }
+            }
+
+                Console.WriteLine("Há {0} positivo(s)", positivo);
+                Console.WriteLine("Há {0} negativo(s)", negativo);
+                Console.WriteLine("Há {0} par(es)", par);
+                Console.WriteLine("Há {0} impar(es)", impar);
+            Console.Clear();
+            Perg();
+        }
+        static void Perg()
+        {
+            string resposta;
+            Console.WriteLine("Deseja encerrar o programa? Se sim digite (S), se não pressione outra tecla");
+            resposta = Console.ReadLine();
+                Console.Clear();
+            if (resposta != "S" && resposta != "s")
+            {
+                Calc();
+             
+            }
+           
+        }
+        }
+    
+
+}
